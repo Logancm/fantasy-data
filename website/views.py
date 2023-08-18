636a1dcac258102e969e1a085ce171a303ee8621
Logancm <milazzo123@outlook.com>
2023-08-17 22:47:27 -0500

    Adp and bootstrap done

--- website/views.py
+++ website/views.py
@@ -12,3 +12,19 @@ def home():
 def adp():
     players = scrape_players_data()
     return render_template('adp.html', players=players)
+
+@views.route('/expert-rankings.html')
+def expertRankings():
+    return render_template("expert-rankings.html")
+
+@views.route('/league-data.html')
+def league_data():
+    return render_template("league-data.html")
+
+@views.route('/stats.html')
+def stats():
+    return render_template("stats.html")
+
+@views.route('/account.html')
+def account():
+    return render_template("account.html")
